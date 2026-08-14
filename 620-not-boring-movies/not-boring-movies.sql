e9bccd581ef7f354162e7d52932f47dbf0440ae9
Olivia <olivia.millarr@gmail.com>
2026-08-14 19:21:25 +0100

    Time: 297 ms (35.77%) | Memory: 0B (100.00%) - LeetSync

---
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT
+    id
+    ,movie
+    ,description
+    ,rating
+FROM cinema
+WHERE description != 'boring'
+    AND id % 2 = 1
+ORDER BY rating DESC;
